dbc752220fa154556b714f0a42fea067bb4e12fc
ZAKARKO <zakizaki442005@ggmail.com>
2024-04-29 03:19:45 +0100

    intializing pygame

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import os
 from PIL import Image
 import subprocess
 # Initialize Pygame
+pygame.init()
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800  # Adjusted height to fit 10x10 grid
+GRID_SIZE = 10  # Size of the invisible grid
+IMAGE_SIZE = 80  # Size of each image
+PIECE_FOLDER = "pieces100"  # Updated folder name
+NUM_IMAGES = 100
